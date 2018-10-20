7f6b01edf2eed6b3a76dbe20fce77108f0d4ee80
zuendorf <zuendorf@uni-kassel.de>
2018-10-20 17:29:31 +0200

    some more api for convenience

--- src/main/java/org/fulib/yaml/YamlObject.java
+++ src/main/java/org/fulib/yaml/YamlObject.java
@@ -4,6 +4,22 @@ import java.util.LinkedHashMap;
 
 public class YamlObject
 {
+
+   public static final String ID = ".id";
+   public static final String TYPE = "type";
+
+
+   public YamlObject()
+   {
+      // empty
+   }
+
+   public YamlObject(String id, String type)
+   {
+      map.put(ID, id);
+      map.put(TYPE, type);
+   }
+
    private LinkedHashMap<String, Object> map = new LinkedHashMap<>();
 
    public LinkedHashMap<String, Object> getMap()
@@ -11,10 +27,21 @@ public class YamlObject
       return map;
    }
 
+   public YamlObject put(String tag, Object value)
+   {
+      map.put(tag, value);
+      return this;
+   }
+
+   public Object get(String tag)
+   {
+      return map.get(tag);
+   }
+
    @Override
    public String toString()
    {
-      Object id = map.get(".id");
+      Object id = map.get(ID);
 
       if (id != null) return id.toString();
 
