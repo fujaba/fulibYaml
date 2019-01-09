3b281a5e9996d2538dce607ce5b39bdeb114272d
zuendorf <zuendorf@uni-kassel.de>
2019-01-10 00:53:30 +0100

    fulib yaml deals with emf now (all via reflection)

--- src/main/java/org/fulib/yaml/ReflectorMap.java
+++ src/main/java/org/fulib/yaml/ReflectorMap.java
@@ -3,12 +3,16 @@ package org.fulib.yaml;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ReflectorMap
 {
    Map<String, Reflector> reflectorMap = new LinkedHashMap<>();
 
    private ArrayList<String> packageNames;
+
+   private Class eObject = null;
 
    public ReflectorMap(String packageName)
    {
@@ -17,11 +21,29 @@ public class ReflectorMap
       packageNames.add(packageName);
 
       this.packageNames = packageNames;
+
+      try
+      {
+         eObject = Class.forName("org.eclipse.emf.ecore.EClass");
+         Logger.getGlobal().log(Level.INFO, "could load org.eclipse.emf.ecore.EClass");
+      }
+      catch (ClassNotFoundException e)
+      {
+      }
    }
 
    public ReflectorMap(ArrayList<String> packageNames)
    {
       this.packageNames = packageNames;
+
+      try
+      {
+         eObject = ClassLoader.getSystemClassLoader().loadClass("org.eclipse.emf.ecore.EObject");
+         Logger.getGlobal().log(Level.INFO, "could load org.eclipse.emf.ecore.EObject");
+      }
+      catch (ClassNotFoundException e)
+      {
+      }
    }
 
 
@@ -43,10 +65,9 @@ public class ReflectorMap
       // already known?
       Reflector reflector = reflectorMap.get(clazzName);
 
-      if (reflector != null)
-      {
-         return reflector;
-      }
+      if (reflector != null) return reflector; //====================
+
+
 
       for (String packageName : packageNames)
       {
@@ -65,7 +86,27 @@ public class ReflectorMap
          }
          catch (Exception e)
          {
-            reflector = null;
+            if (eObject == null) return null; //=======================
+
+            try
+            {
+               fullClassName = packageName + ".impl." + clazzName;
+
+               Class<?> theClass = Class.forName(fullClassName);
+
+               if (theClass != null)
+               {
+                  reflector = new Reflector()
+                        .setClassName(fullClassName)
+                        .setUseEMF();
+                  reflectorMap.put(clazzName, reflector);
+                  return reflector;
+               }
+            }
+            catch (Exception e2)
+            {
+               reflector = null;
+            }
          }
       }
 
