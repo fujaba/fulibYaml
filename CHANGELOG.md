29ce07240464bfbc2bf179ee30b80f52e60805f4
Adrian Kunz <clashsoft@hotmail.com>
2019-09-24 15:32:42 +0200

    Use Simple-Publish

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,3 +5,7 @@
 # fulibYaml v1.0.2
 
 # fulibYaml v1.0.3
+
+# fulibYaml v1.1.0
+
+* Bumped version number.
--- travis-deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-  openssl aes-256-cbc -K $encrypted_0d3d89390900_key -iv $encrypted_0d3d89390900_iv -in my.travis.gpg.enc -out my.travis.gpg -d
-  ./gradlew uploadArchives -PossrhUsername=${SONATYPE_USERNAME} -PossrhPassword=${SONATYPE_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=my.travis.gpg
-fi
