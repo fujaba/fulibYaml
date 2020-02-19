0eac11f5cfd2eadbb1ac86d77ed1aab8272ad03f
Adrian Kunz <clashsoft@hotmail.com>
2020-02-19 18:36:23 +0100

    Add JavaDoc to StrUtil

--- src/main/java/org/fulib/yaml/StrUtil.java
+++ src/main/java/org/fulib/yaml/StrUtil.java
@@ -2,11 +2,23 @@ package org.fulib.yaml;
 
 import java.util.Objects;
 
+/**
+ * This class provides static utility methods for working with strings in general, as used by various implementations
+ * in this library.
+ */
 public class StrUtil
 {
-   public static String cap(String oldTxt)
+   /**
+    * Transforms the first character of the input string to uppercase, leaving the remaining characters as-is.
+    *
+    * @param string
+    *    the input string
+    *
+    * @return the input string with the first character in uppercase
+    */
+   public static String cap(String string)
    {
-      final StringBuilder builder = new StringBuilder(oldTxt);
+      final StringBuilder builder = new StringBuilder(string);
       builder.setCharAt(0, Character.toUpperCase(builder.charAt(0)));
       return builder.toString();
    }
@@ -29,9 +41,17 @@ public class StrUtil
       return Objects.equals(word1, word2);
    }
 
-   public static String downFirstChar(String oldTxt)
+   /**
+    * Transforms the first character of the input string to lowercase, leaving the remaining characters as-is.
+    *
+    * @param string
+    *    the input string
+    *
+    * @return the input string with the first character in lowercase
+    */
+   public static String downFirstChar(String string)
    {
-      final StringBuilder builder = new StringBuilder(oldTxt);
+      final StringBuilder builder = new StringBuilder(string);
       builder.setCharAt(0, Character.toLowerCase(builder.charAt(0)));
       return builder.toString();
    }
