dd10b30ee26bf63d5607657f60e220efa620dcfd
Adrian Kunz <clashsoft@hotmail.com>
2020-02-19 18:33:06 +0100

    Add JavaDoc to YamlObject

--- src/main/java/org/fulib/yaml/YamlObject.java
+++ src/main/java/org/fulib/yaml/YamlObject.java
@@ -5,6 +5,11 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * A container for arbitrary attributes.
+ * Simple attributes can be set with {@link #put(String, Object)} and get with {@link #get(String)}.
+ * Collection attributes can be created with {@link #with(String, Object)}.
+ */
 public class YamlObject
 {
    // =============== Constants ===============
@@ -24,6 +29,9 @@ public class YamlObject
    }
 
    /**
+    * @param id
+    *    the {@link #setId(String) id}
+    *
     * @since 1.2
     */
    public YamlObject(String id)
@@ -31,6 +39,12 @@ public class YamlObject
       this.setId(id);
    }
 
+   /**
+    * @param id
+    *    the {@link #setId(String) id}
+    * @param type
+    *    the {@link #setType(String) type}
+    */
    public YamlObject(String id, String type)
    {
       this(id);
@@ -40,7 +54,7 @@ public class YamlObject
    // =============== Properties ===============
 
    /**
-    * @return the properties
+    * @return the map of property names to property values
     *
     * @deprecated since 1.2; use {@link #getProperties()} instead
     */
@@ -51,7 +65,7 @@ public class YamlObject
    }
 
    /**
-    * @return the properties
+    * @return the map of property names to property values
     *
     * @since 1.2
     */
@@ -61,6 +75,16 @@ public class YamlObject
    }
 
    /**
+    * Convenience getter for {@link #get(String) get}({@link #ID}).
+    * In particular, roughly implemented as
+    *
+    * <pre><code>
+    *    final Object id = this.get(ID);
+    *    return id != null ? id.toString() : null;
+    * </code></pre>
+    *
+    * @return the ID
+    *
     * @since 1.2
     */
    public String getId()
@@ -70,6 +94,11 @@ public class YamlObject
    }
 
    /**
+    * Convenience setter for {@link #put(String, Object) put}({@link #ID}, {@code id}).
+    *
+    * @param id
+    *    the ID
+    *
     * @since 1.2
     */
    public void setId(String id)
@@ -78,6 +107,16 @@ public class YamlObject
    }
 
    /**
+    * Convenience getter for {@link #get(String) get}({@link #TYPE}).
+    * In particular, roughly implemented as
+    *
+    * <pre><code>
+    *    final Object type = this.get(TYPE);
+    *    return type != null ? type.toString() : null;
+    * </code></pre>
+    *
+    * @return the type
+    *
     * @since 1.2
     */
    public String getType()
@@ -87,6 +126,11 @@ public class YamlObject
    }
 
    /**
+    * Convenience setter for {@link #put(String, Object) put}({@link #TYPE}, {@code type}).
+    *
+    * @param type
+    *    the type
+    *
     * @since 1.2
     */
    public void setType(String type)
@@ -96,17 +140,52 @@ public class YamlObject
 
    // =============== Methods ===============
 
+   /**
+    * @param property
+    *    the property name
+    *
+    * @return the attribute value for the given property name, or {@code null} if not specified
+    */
    public Object get(String property)
    {
       return this.map.get(property);
    }
 
+   /**
+    * Sets the attribute value for the given property name.
+    *
+    * @param property
+    *    the property name
+    * @param value
+    *    the attribute value
+    *
+    * @return this instance, to allow method chaining
+    */
    public YamlObject put(String property, Object value)
    {
       this.map.put(property, value);
       return this;
    }
 
+   /**
+    * Adds the item to the collection attribute with the given property name.
+    * In particular, the implementation is as follows:
+    *
+    * <ol>
+    *   <li>If the property is not set, set the item as the attribute value as if
+    *   {@link #put(String, Object)}({@code property}, {@code item}) was called</li>
+    *   <li>If the property is already set and the value is a {@link List}, add the {@code item} to that list</li>
+    *   <li>If the property is already set and the value is not a {@link List},
+    *   create a new list with the old value and the {@code item} and set that as the new value</li>
+    * </ul>
+    *
+    * @param property
+    *    the property name
+    * @param item
+    *    the item to add or set
+    *
+    * @return this instance, to allow method chaining
+    */
    public YamlObject with(String property, Object item)
    {
       this.map.compute(property, (k, oldValue) -> {
